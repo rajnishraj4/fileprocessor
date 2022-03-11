1cc938eb5abfe9b65b9462c42d036b26777304fd
Rajnish Raj <rajnishraj4@gmail.com>
2022-03-12 01:04:11 +0530

    commit all

---
+++ src/main/java/com/processor/file/flat/fileprocessor/FileprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.processor.file.flat.fileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileprocessorApplication.class, args);
+	}
+
+}
